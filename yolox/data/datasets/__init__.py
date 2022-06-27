0c737346f1a757854e3b8f08f1cd56974ae11154
AhmedKhaled945 <a.khaled.taha94@gmail.com>
2022-06-27 14:49:43 +0200

    Update __init__.py

--- yolox/data/datasets/__init__.py
+++ yolox/data/datasets/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 # Copyright (c) Megvii, Inc. and its affiliates.
 
-from .mot import MOTDataset
+#from .mot import MOTDataset
 from .coco import COCODataset
 from .coco_classes import COCO_CLASSES
 from .datasets_wrapper import ConcatDataset, Dataset, MixConcatDataset
